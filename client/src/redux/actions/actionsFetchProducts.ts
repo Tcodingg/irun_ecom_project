479372b5f7660787237bc258921dee217df838ac
Tcodingg <princetlive@yahoo.com>
2021-12-22 18:21:05 -0500

    write redux action funtions that fetechs the products

--- client/src/redux/actions/actionsFetchProducts.ts
+++ client/src/redux/actions/actionsFetchProducts.ts
@@ -2,9 +2,10 @@ import { actionTypes } from "./actionTypes.";
 import { Dispatch } from "redux";
 import axios from "axios";
 
+const url: any = process.env.REACT_APP_SERVER_URL;
 // ==== GET ALL PRODUCTS ==== //
+
 export const getAllProducts = () => async (dispatch: Dispatch) => {
-   const url: any = process.env.REACT_APP_SERVER_URL;
    dispatch({
       type: actionTypes.GET_ALL_PRODUCTS_LOADING,
    });
@@ -16,6 +17,27 @@ export const getAllProducts = () => async (dispatch: Dispatch) => {
       });
 
       console.log(data);
+   } catch (error) {
+      dispatch({
+         type: actionTypes.GET_ALL_PRODUCTS_FAIL,
+         payload: error,
+      });
+   }
+};
+
+//   GET A PRODUCT
+
+export const getProduct = (id: string) => async (dispatch: Dispatch) => {
+   dispatch({
+      type: actionTypes.GET_PRODUCT_LOADING,
+   });
+
+   try {
+      const { data } = await axios.get(`${url}/api/products$/{id}`);
+      dispatch({
+         type: actionTypes.GET_PRODUCT_SUCCESS,
+         payload: data,
+      });
    } catch (error) {
       dispatch({
          type: actionTypes.GET_ALL_PRODUCTS_FAIL,
