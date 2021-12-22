1d4f25981c7fdf0cb3f18bad46c8075779f7280e
Tcodingg <princetlive@yahoo.com>
2021-12-22 14:31:01 -0500

    fix typo

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
       <section className="home">
          <main className="main-container">
             <div className="bd-container main-info">
-               <h1 className="title">flash sale of 70%</h1>
+               <h1 className="title">flash sale 70% off</h1>
                <p>
                   Fashion is part of the daily air and it changes all the time,
                   with all the events. You can see and feel everything in
