0888e237df67eeb29212115cd2d191aba9a3b408
Tcodingg <princetlive@yahoo.com>
2021-12-24 17:17:36 -0500

    install swiper. create products slide component and style it.

---
+++ client/src/components/ProductSlides/ProductSlides.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+import "./ProductSlides.scss";
+
+import SwiperCore, { FreeMode, Navigation, Thumbs } from "swiper";
+
+SwiperCore.use([FreeMode, Navigation, Thumbs]);
+
+interface imagesInterface {
+   images: string[];
+}
+const ProductSlides: React.FC<imagesInterface> = ({ images }) => {
+   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperCore>();
+   return (
+      <>
+         <Swiper
+            spaceBetween={10}
+            // navigation={true}
+            thumbs={{ swiper: thumbsSwiper }}
+            className="mySwiper2"
+         >
+            {images.map((i) => {
+               return (
+                  <SwiperSlide>
+                     <img src={i} alt="" />
+                  </SwiperSlide>
+               );
+            })}
+         </Swiper>
+         <Swiper
+            onSwiper={setThumbsSwiper}
+            spaceBetween={10}
+            slidesPerView={4}
+            freeMode={true}
+            watchSlidesProgress={true}
+            className="mySwiper"
+         >
+            {images.map((i) => {
+               return (
+                  <SwiperSlide>
+                     <img src={i} alt="" />
+                  </SwiperSlide>
+               );
+            })}
+         </Swiper>
+      </>
+   );
+};
+
+export default ProductSlides;
--- client/src/pages/product/Product.tsx
+++ client/src/pages/product/Product.tsx
@@ -4,19 +4,38 @@ import { RootState } from "../../redux/rootReducer";
 import { useSelector, useDispatch } from "react-redux";
 import { getProduct } from "../../redux/actions/actionsFetchProducts";
 
+import "./Product.scss";
+import ProductSlides from "../../components/ProductSlides/ProductSlides";
+import Button from "../../components/button/Button";
+
 const Product: React.FC = () => {
    const dispatch = useDispatch();
+   const { id } = useParams();
+
    const product = useSelector(
       (state: RootState) => state.productReducer.product
    );
-   const { id } = useParams();
+
    useEffect(() => {
       dispatch(getProduct(id));
    }, [id, dispatch]);
-   console.log(product);
+
    return (
       <section className="product">
-         <div className="bd-container product-container"></div>
+         <div className="bd-container product-container">
+            <div className="product-wrapper">
+               <div>
+                  <ProductSlides images={product.images} />
+               </div>
+               <div>
+                  <p className="text">
+                     {product.title}
+                     <p>{`$${product.price}`}</p>
+                     <button>add to cart</button>
+                  </p>
+               </div>
+            </div>
+         </div>
       </section>
    );
 };
