31e64bdf74b6bd3f60b6e497a2e29632fd243870
Tcodingg <princetlive@yahoo.com>
2021-12-27 18:14:14 -0500

    replace the chekcout dummy values with the actual values

--- client/src/pages/cart/Cart.tsx
+++ client/src/pages/cart/Cart.tsx
@@ -1,12 +1,35 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import CartItem from "../../components/cartItem/CartItem";
 import { RootState } from "../../redux/rootReducer";
 import "./Cart.scss";
+
+interface checkoutInterface {
+   subTotal: number;
+   taxes: number;
+   total: number;
+}
 const Cart: React.FC = () => {
+   const [checkout, setCheckout] = useState<checkoutInterface>({
+      subTotal: 0,
+      taxes: 0,
+      total: 0,
+   });
    const cart = useSelector((state: RootState) => state.cartReducer.cart);
-
-   console.log(cart);
-
+   const subTotal: number = cart.reduce(
+      (amount, item) => item.price * item.qty + amount,
+      0
+   );
+   const taxes: number = subTotal * 0.13;
+   const total = subTotal + taxes;
+   useEffect(() => {
+      setCheckout({
+         subTotal: Number(subTotal.toFixed(2)),
+         taxes: Number(taxes.toFixed(2)),
+         total: Number(total.toFixed(2)),
+      });
+   }, [subTotal, taxes, total]);
+   console.log(checkout.subTotal);
    return (
       <section className="cart">
          <div className="bd-container cart-container">
@@ -34,13 +57,15 @@ const Cart: React.FC = () => {
                         <p className="text">taxes</p>
                      </div>
                      <div className="checkout-right">
-                        <p className="text">$1390</p>
-                        <p className="text">$20</p>
+                        <p className="text">{`$${checkout.subTotal.toFixed(
+                           2
+                        )}`}</p>
+                        <p className="text">{`$${checkout.taxes}`}</p>
                      </div>
                   </div>
                   <div className="checkout-bottom">
                      <p className="text">total</p>
-                     <p className="text">$1270</p>
+                     <p className="text">{`$${checkout.total}`}</p>
                   </div>
                </div>
                <div className="checkout-btn-container">
