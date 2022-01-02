5040daf5d45e5ca4d442c0d98b89e1d047bb4bbd
Tcodingg <princetlive@yahoo.com>
2022-01-02 15:23:26 -0500

    add the register auth action

--- client/src/redux/actions/auth/authActions.ts
+++ client/src/redux/actions/auth/authActions.ts
@@ -4,6 +4,7 @@ import { actionTypes, Actions } from "./authTypes";
 
 const url = process.env.REACT_APP_SERVER_URL;
 
+// login
 export const login = (input: any) => async (dispatch: Dispatch<Actions>) => {
    try {
       const { data } = await axios.get(`${url}/auth/login`, input);
@@ -14,6 +15,23 @@ export const login = (input: any) => async (dispatch: Dispatch<Actions>) => {
    } catch (error) {
       dispatch({
          type: actionTypes.LOGIN_FAIL,
+         payload: error,
+      });
+   }
+};
+
+// register
+
+export const register = (input: any) => async (dispatch: Dispatch) => {
+   try {
+      const { data } = await axios.get(`${url}/api/register`, input);
+      dispatch({
+         type: actionTypes.REGISTER_SUCCESS,
+         payload: data,
+      });
+   } catch (error) {
+      dispatch({
+         type: actionTypes.REGISTER_FAIL,
          payload: error,
       });
    }
