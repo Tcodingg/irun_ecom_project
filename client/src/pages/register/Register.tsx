4ddbf424e619b9100f3357ea6c39430d0a93a277
Tcodingg <princetlive@yahoo.com>
2022-01-09 18:02:10 -0500

    validate form as users are typing

--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import Button from "../../components/button/squareButtons/Button";
 import { register } from "../../redux/actions/auth/authActions";
@@ -12,10 +12,11 @@ const Register: React.FC = () => {
       confirmPassword: "",
    });
 
-   const [isValidName, setIsValidName] = useState(null);
-   const [isValidEmail, setIsValidEmail] = useState(null);
-   const [isValidPassword, setIsValidPassword] = useState(null);
-   const [isValidConfirmPassword, setIsValidConfirmPassword] = useState(null);
+   const [isValidName, setIsValidName] = useState<any>(null);
+   const [isValidEmail, setIsValidEmail] = useState<any>(null);
+   const [isValidPassword, setIsValidPassword] = useState<any>(null);
+   const [isValidConfirmPassword, setIsValidConfirmPassword] =
+      useState<any>(null);
    // handle input values
    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       const { name, value } = e.target;
@@ -25,11 +26,54 @@ const Register: React.FC = () => {
       });
    };
 
+   // validate input fields
+   const regexp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+   useEffect(() => {
+      function validateInput() {
+         // validate name
+         input.name.length === 0
+            ? setIsValidName(null)
+            : input.name.length > 4
+            ? setIsValidName(true)
+            : setIsValidName(false);
+
+         // validate password
+         input.password.length === 0
+            ? setIsValidPassword(null)
+            : input.password.length < 6
+            ? setIsValidPassword(false)
+            : setIsValidPassword(true);
+
+         // validate confirm password
+         input.confirmPassword.length === 0
+            ? setIsValidConfirmPassword(null)
+            : input.password === input.confirmPassword
+            ? setIsValidConfirmPassword(true)
+            : setIsValidConfirmPassword(false);
+         // validate email
+         input.email.length === 0
+            ? setIsValidEmail(null)
+            : regexp.test(input.email)
+            ? setIsValidEmail(true)
+            : setIsValidEmail(false);
+      }
+      validateInput();
+   }, [input.name, input.email, input.confirmPassword, input.password]);
    // handle register
    const handleRegister = () => {
       dispatch(register(input));
    };
 
+   console.log(
+      isValidPassword,
+      isValidEmail,
+      isValidConfirmPassword,
+      isValidName
+   );
+
+   console.log(input.password);
    return (
       <section className="register">
          <div className="bd-container register-container section flex">
@@ -42,7 +86,11 @@ const Register: React.FC = () => {
                      placeholder="Name *"
                      name="name"
                   />
-                  <small>Please enter a valid name!</small>
+                  {isValidName === false ? (
+                     <small>Please enter a valid name!</small>
+                  ) : (
+                     ""
+                  )}
                </div>
                <div className="form-control">
                   <input
@@ -51,7 +99,11 @@ const Register: React.FC = () => {
                      placeholder="Email *"
                      name="email"
                   />
-                  <small>Please enter a valid email!</small>
+                  {isValidEmail === false ? (
+                     <small>Please enter a valid email!</small>
+                  ) : (
+                     ""
+                  )}
                </div>
 
                <div className="form-control">
@@ -61,7 +113,11 @@ const Register: React.FC = () => {
                      placeholder="Password *"
                      name="password"
                   />
-                  <small>Please enter a valid password!</small>
+                  {isValidPassword === false ? (
+                     <small>Please enter a valid password!</small>
+                  ) : (
+                     ""
+                  )}
                </div>
                <div className="form-control">
                   <input
@@ -70,7 +126,11 @@ const Register: React.FC = () => {
                      placeholder="Confirm Password *"
                      name="confirmPassword"
                   />
-                  <small>Password did not match!</small>
+                  {isValidConfirmPassword === false ? (
+                     <small>Password did not match!</small>
+                  ) : (
+                     ""
+                  )}
                </div>
                <Button handleClick={handleRegister} text="create an account" />
             </div>
